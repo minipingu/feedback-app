3acd6e447e7ec5287b7363a95231b75f81d5ba0a
Dede Nurhidayat <ranmaniax@gmail.com>
2023-11-21 19:15:01 +0700

    010 Conditionals in JSX

--- src/App.js
+++ src/App.js
@@ -16,18 +16,29 @@ function App() {
 			text: 'comment three',
 		},
 	]
+
+	const loading = false
+	const showComments = true
+	//conditional rendering in function component using if
+	if (loading) return <h1>Loading...</h1>
+
+	const commentBlock = (
+		<div className='comments'>
+			<h3>Comments ({comments.length})</h3>
+			<ul>
+				{comments.map((comment, index) => (
+					<li key={comment.id}>{comment.text}</li>
+				))}
+			</ul>
+		</div>
+	)
+
 	return (
 		<div className='container'>
 			<h1>{title.toUpperCase()}</h1>
 			<p>{body}</p>
-			<div className='comments'>
-				<h3>Comments ({comments.length})</h3>
-				<ul>
-					{comments.map((comment, index) => (
-						<li key={comment.id}>{comment.text}</li>
-					))}
-				</ul>
-			</div>
+			{/* conditional rendering inside jsx using ternary*/}
+			{showComments && commentBlock}
 		</div>
 	)
 }
