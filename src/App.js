71903a4d7ebeb08b2b2629d4e29ca52d09ab047e
Dede Nurhidayat <ranmaniax@gmail.com>
2023-11-21 18:55:46 +0700

    009 Dynamic values and lists in jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const title = 'Blog Post'
+	const body = 'This is my blog post'
+	const comments = [
+		{
+			id: 1,
+			text: 'comment one',
+		},
+		{
+			id: 2,
+			text: 'comment two',
+		},
+		{
+			id: 3,
+			text: 'comment three',
+		},
+	]
+	return (
+		<div className='container'>
+			<h1>{title.toUpperCase()}</h1>
+			<p>{body}</p>
+			<div className='comments'>
+				<h3>Comments ({comments.length})</h3>
+				<ul>
+					{comments.map((comment, index) => (
+						<li key={comment.id}>{comment.text}</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
