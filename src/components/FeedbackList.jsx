d7c06bea909d221b2a90c084ee40c614a8f4693a
Dede Nurhidayat <ranmaniax@gmail.com>
2023-11-22 13:09:17 +0700

    Card Component and Conditional Styles

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import FeedbackItem from './FeedbackItem'
+import PropTypes from 'prop-types'
 function FeedbackList({ feedback }) {
 	if (!feedback || feedback.length === 0) {
 		return <p>No Feedback Yet</p>
@@ -10,6 +11,16 @@ function FeedbackList({ feedback }) {
 			))}
 		</div>
 	)
+}
+
+FeedbackList.propTypes = {
+	feedback: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.number.isRequired,
+			text: PropTypes.string.isRequired,
+			rating: PropTypes.number.isRequired,
+		})
+	),
 }
 
 export default FeedbackList
