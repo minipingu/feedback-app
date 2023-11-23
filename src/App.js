9d93f66bb853bf344c4156984b58e3a69779dda3
Dede Nurhidayat <ranmaniax@gmail.com>
2023-11-23 17:59:22 +0700

    Optional Link to object

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import FeedbackData from './components/data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import AboutPage from './pages/AboutPage'
+import AboutIconLink from './components/AboutIconLink'
 function App() {
 	const [feedback, setFeedback] = useState(FeedbackData)
 
@@ -41,6 +42,7 @@ function App() {
 						}></Route>
 					<Route path='/about' element={<AboutPage />} />
 				</Routes>
+				<AboutIconLink />
 			</div>
 		</Router>
 	)
